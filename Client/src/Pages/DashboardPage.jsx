53001898f92d54812b76098010b4691c280370e3
GabS744 <sousagabriel7444@gmail.com>
2026-08-11 02:25:59 -0300

    feat(components): Creation of the Navbar component and configuration of the Default Layout.

---
+++ Client/src/Pages/DashboardPage.jsx
@@ -0,0 +1,13 @@
+import { DefaultLayout } from "../Components/DefaultLayout";
+
+export function DashboardPage() {
+  return (
+    <>
+      <DefaultLayout />
+      <div>
+        <h1>Dashboard de Teste</h1>
+        <p>Se você está vendo isso, a página está funcionando.</p>
+      </div>
+    </>
+  );
+}
